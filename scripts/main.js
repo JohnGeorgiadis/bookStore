e601b9691103d5bbd2f8dd7c478f64d3de0841d4
JohnGeorgiadis <j.georgiadiss@gmail.com>
2017-09-18 22:11:01 +0200

    fb promt at the last page

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,31 @@ $(document).ready(function(){
     $("#flipbook").turn({
         width: 960,
         height: 600
-    },"center");
+        },"center")
+        .bind("turning", function(event, page, view) {
+        if (page==1) {
+            event.preventDefault(); //will not happen at page 1
+        }else{
+            console.log("Turning the page to: "+page);
+        }
+    });
 
+    $("#share-button").click(function(){
+        FB.ui(
+            {
+                method: 'share',
+                href: 'https://developers.facebook.com/docs/',
+                app_id: '206540289883972'
+
+            },
+            // callback
+            function(response) {
+                if (response && !response.error_message) {
+                    alert('Posting completed.');
+                } else {
+                    alert('Error while posting.');
+                }
+            }
+        );
+    })
 });
