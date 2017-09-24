b59255dcdd12edcc9464f72195e7b7d62c8ba2be
JohnGeorgiadis <j.georgiadiss@gmail.com>
2017-09-24 02:30:30 +0200

    desing updates

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,19 @@ $(document).ready(function(){
 
     $("#flipbook").turn({
         width: 960,
-        height: 600
+        height: 700
         },"center")
         .bind("turning", function(event, page) {
-            if (page===6) {
+            console.log(page)
+            if (page===1) {
+                $(".outer-layer").css("display", "none");
+                $(".depth-effect").css("display", "inline-block");
+            }
+            if (page===2) {
+                $(".outer-layer").css("display", "inline-block");
+                $(".depth-effect").css("display", "none");
+            }
+            if (page===22) {
                 $(".share-button").click(function() {
                     FB.ui(
                         {
@@ -26,16 +35,17 @@ $(document).ready(function(){
                         }
                     );
                 })
-            } else{
-                console.log("Turning the page to: "+page);
+            }
+            if (page===24) {
+                event.preventDefault();
             }
         });
 
-    $.getJSON('https://gist.githubusercontent.com/JohnGeorgiadis/d3bf915dc51d8e67622b7964d891caeb/raw/e1dfffe30ef513aad47f3cfa81c1c03918d711e8/witkowski',
-        function(data) {
-            $.each(data.pages, function(index, value) {
-                $( ".page-"+index ).append(value);
-            })
-    });
+    // $.getJSON('https://gist.githubusercontent.com/JohnGeorgiadis/d3bf915dc51d8e67622b7964d891caeb/raw/e1dfffe30ef513aad47f3cfa81c1c03918d711e8/witkowski',
+    //     function(data) {
+    //         $.each(data.pages, function(index, value) {
+    //             $( ".page-"+index ).append(value);
+    //         })
+    // });
 
 });
